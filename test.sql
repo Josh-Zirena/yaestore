cd77d8fbc1e64446ee869fb289aafdb86af866a9
Josh Zirena <JoshZirena@gmail.com>
2020-06-22 10:26:08 +0200

    feat: added support for postgres in nodejs

--- test.sql
+++ test.sql
@@ -1,10 +1,46 @@
 DROP TABLE IF EXISTS Products;
 
 CREATE Table Products(
-    ProductID INT(9) NOT NULL AUTO_INCREMENT,
+    ProductID INT PRIMARY KEY,
     ProductName VARCHAR(200) NOT NULL,
-    BrandID INT(9) NOT NULL,
-    CategoryID INT(9) NOT NULL,
-    ModelYear INT(4),
-    ListPrice INT(9)              
-)
+    BrandID INT NOT NULL,
+    CategoryID INT NOT NULL,
+    ModelYear INT,
+    ListPrice INT            
+);
+
+INSERT INTO Products VALUES (
+    000000001,
+    'Diamond Earrings Black/White',
+    253809535,
+    134123124,
+    1990,
+    1494.99
+);
+
+INSERT INTO Products VALUES (
+    000000002,
+    'Diamond Earrings Black/White',
+    253809535,
+    134123124,
+    1990,
+    1494.99
+);
+
+INSERT INTO Products VALUES (
+    000000003,
+    'Diamond Ring White',
+    253809535,
+    134123124,
+    1990,
+    2894.99
+);
+
+INSERT INTO Products VALUES (
+    000000004,
+    'Rolex Watch Sumarine',
+    253809535,
+    134123124,
+    1990,
+    3294.99
+);
